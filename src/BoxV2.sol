6953836433492cd9bb286df3cd1c0e6b5e3643c2
AtulMinz <atulminz.jsr@gmail.com>
2025-02-20 18:44:25 +0530

    BoxV2 Initialized

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,14 +1,12 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.26;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
-    uint256 internal value;
-
-    // To disable initializer after deployment
+contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     constructor() {
         _disableInitializers();
     }
@@ -18,13 +16,7 @@ contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         __UUPSUpgradeable_init();
     }
 
-    function getValue() public view returns (uint256) {
-        return value;
-    }
-
-    function version() public pure returns (string memory) {
-        return "BoxV1";
-    }
+    function
 
     function _authorizeUpgrade(address newImplementation) internal override {}
 }
