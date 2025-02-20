484f29937873f2d101a4fe54293dc4316df43b7d
AtulMinz <atulminz.jsr@gmail.com>
2025-02-20 17:57:51 +0530

    Initialize Proxy contract

---
+++ src/BoxV1.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+abstract contract BoxV1 is UUPSUpgradeable {}
