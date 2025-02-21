b5cd1204c0c2314f9ec657735adb09896098756f
AtulMinz <atulminz.jsr@gmail.com>
2025-02-21 22:31:48 +0530

    Added upgradeable contract

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -5,18 +5,19 @@ pragma solidity 0.8.26;
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {BoxV1} from "./BoxV1.sol";
 
-contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract BoxV2 is BoxV1 {
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() public initializer {
+    function initialize() public override initializer {
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
     }
 
-    function
+    //function
 
     function _authorizeUpgrade(address newImplementation) internal override {}
 }
