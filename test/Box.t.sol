1b2c846b024caab65cbf7df4cab2c5a6be018fcc
AtulMinz <atulminz.jsr@gmail.com>
2025-03-11 06:53:15 +0000

    Merged

--- test/Box.t.sol
+++ test/Box.t.sol
@@ -3,5 +3,14 @@
 pragma solidity 0.8.26;
 
 import {Test} from "forge-std/Test.sol";
+import {BoxV1} from "../src/BoxV1.sol";
+import {BoxV2} from "../src/BoxV2.sol";
 
-contract BoxTest is Test{}
+contract BoxTest is Test{
+    BoxV1 boxV1;
+    BoxV2 boxV2;
+
+    function setUp() external {
+        
+    }
+}
